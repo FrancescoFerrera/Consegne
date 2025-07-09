3689ad71fb2b26c88a6835bcd4b749b248fc3795
FrancescoFerrera <fferrera2003@gmail.com>
2025-07-09 15:26:21 +0200

    Create Pratica S2L3.py

---
+++ UNIT1/S2/L3/Pratica S2L3.py
@@ -0,0 +1,5 @@
+citta = input("Qual è il nome della tua città?: ")
+animale = input("Qual è il nome del tuo animale domestico?: ")
+
+band = (citta + ' ' + animale)
+print("Il nome della tua band è", band)
